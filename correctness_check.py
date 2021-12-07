ab1455b40c430f610107af9b8785ff01bb8619bb
jfr2102 <johannes@friedlein.eu>
2021-12-08 00:39:25 +0100

    improve correctness_check, fix style

--- correctness_check.py
+++ correctness_check.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import json
+import os
+import sys
 from input import *
 pd.options.mode.chained_assignment = None
 path = "/home/jfr/Thesis/kafka-logs/"
@@ -16,6 +18,9 @@ def equalMessageTotal(df):
 
 def equalMessagePerWindow(df):
     benchmark_result, correctness_result = result_split(df)
+    if len(correctness_result.index) == 0:
+        print("\tequalMessagePerWindow Test Failed:", "Empty check DataFrame")
+        return False
     for row in benchmark_result.iterrows():
         row_value = row[1]
         record_key = row_value[" record.key"]
@@ -26,32 +31,54 @@ def equalMessagePerWindow(df):
                                      & (correctness_result[" window_size"] == window_size)]
 
         if len(matches) != 1:
+            print("\tequalMessagePerWindow Test Failed:",
+                  record_key, partition, window_size)
             return False
     return True
 
+
 def equalRecordValue(df):
     benchmark_result, correctness_result = result_split(df)
-    for row in benchmark_result[0:12].iterrows():
+    if len(correctness_result.index) == 0:
+        print("\teequalRecordValue failed:", "Empty check DataFrame")
+        return False
+    for row in correctness_result.iterrows():
         row_value = row[1]
         record_key = row_value[" record.key"]
         partition = row_value[" partition"]
         record_value = row_value[" record.value"]
         count_per_city = json.loads(record_value)["count_per_city"]
 
-        correctness_result["count_per_city"] = correctness_result[" record.value"].apply(lambda x: json.loads(x)["count_per_city"])
-        matches = correctness_result[(correctness_result[" record.key"] == record_key)
-                                     & (correctness_result[" partition"] == partition)
-                                     & (correctness_result["count_per_city"] == count_per_city)
-                                    ]
+        benchmark_result["count_per_city"] = benchmark_result[" record.value"].apply(
+            lambda x: json.loads(x)["count_per_city"])
+        matches = benchmark_result[(benchmark_result[" record.key"] == record_key)
+                                   & (benchmark_result[" partition"] == partition)
+                                   & (benchmark_result["count_per_city"] == count_per_city)
+                                   ]
         if len(matches) != 1:
+            print("\tequalRecordValue Test Failed:",
+                  record_key, partition, count_per_city)
             return False
     return True
 
+def testFun():
+    return True
+    return False
+
 for run in runs:
-    print(run)
+
+    try:
+        os.mkdir("outputs/"+run+"/")
+    except FileExistsError:
+        pass
+    out = open('outputs/' + run + '/correctness_output.txt', 'w')
+
+    print(run, ":")
     filename = path + run + "/resultsoutput.csv"
     df = pd.read_csv(filename, sep=";")
     # Number of messages equal?
-    print("Equal message count total:", equalMessageTotal(df))
-    print("Equal message count per window:", equalMessagePerWindow(df))
-    print("Equal message count per city per window:", equalRecordValue(df))
+    print("Equal message count total:", equalMessageTotal(df),
+    "\nEqual message count per window:", equalMessagePerWindow(df),
+    "\nEqual message count per city per window:", equalRecordValue(df),
+    file = out)
+    print(testFun())
--- latency.py
+++ latency.py
@@ -12,60 +12,72 @@ import sys
 import os
 import matplotlib.ticker as mtick
 
-def annotate(ax = None):
+
+def annotate(ax=None):
     if ax is None:
         ax = pyplot
     ax.axvline(x=fault_begin, color='r', linestyle=':')
     ax.axvline(x=fault_end, color='r', linestyle=':')
-    ax.axvline(x=start_benchmark, color="g", linestyle = "-")
+    ax.axvline(x=start_benchmark, color="g", linestyle="-")
 
-def set_axis(ax, ylabel = None):
+
+def set_axis(ax, ylabel=None):
     if ylabel is not None:
         ax.set_ylabel(ylabel)
     ax.get_xaxis().set_major_formatter(date_fmt)
-    ax.set_xticks([start_check, start_benchmark, fault_end, fault_end + timedelta(minutes=0.5)])
+    ax.set_xticks([start_check, start_benchmark, fault_end,
+                  fault_end + timedelta(minutes=0.5)])
     ax.set_xticklabels(["t_0", "t_1   t_2", "t_3", ""])
+
 
 timepattern = "%Y-%m-%d-%H-%M-%S"
 sns.set()
 date_fmt = mdates.DateFormatter('%H:%M:%S')
-path="/home/jfr/Thesis/kafka-logs/"
+path = "/home/jfr/Thesis/kafka-logs/"
 
-for run in runs: 
-    filename= path + run + "/resultsoutput.csv"
+for run in runs:
+    filename = path + run + "/resultsoutput.csv"
     producerfilename = path + run + "/produceroutput.csv"
     testdriverfilename = path + run + "/testdriverinfo.txt"
 
     # todo: load benchmark and check_begin from produceroutput.csv file?
     producerlog = pd.read_csv(producerfilename, sep=";")
-    benchmark_begin_loaded = (producerlog.loc[lambda df: df['note'] == "benchmark",:])['timestamp'].iloc[0]
-    check_begin_loaded = (producerlog.loc[lambda df: df['note'] == "check",:])['timestamp'].iloc[0]
+    benchmark_begin_loaded = (
+        producerlog.loc[lambda df: df['note'] == "benchmark", :])['timestamp'].iloc[0]
+    check_begin_loaded = (producerlog.loc[lambda df: df['note'] == "check", :])[
+        'timestamp'].iloc[0]
 
     testdriverinfo = pd.read_csv(testdriverfilename, sep=";")
     fault_begin_loaded = testdriverinfo["failure_start"].iloc[0]
     fault_end_loaded = testdriverinfo["failure_end"].iloc[0]
 
-    fault_begin = datetime.strptime(fault_begin_loaded, timepattern) #- timedelta(hours = 1)
-    fault_end = datetime.strptime(fault_end_loaded, timepattern) #- timedelta(hours = 1)
-   
-    start_benchmark = pd.to_datetime(benchmark_begin_loaded, unit='ms').to_pydatetime()
-    start_check = pd.to_datetime(check_begin_loaded, unit='ms').to_pydatetime()
-    print(start_benchmark, fault_begin, fault_end)
+    fault_begin = datetime.strptime(
+        fault_begin_loaded, timepattern)  # - timedelta(hours = 1)
+    fault_end = datetime.strptime(
+        fault_end_loaded, timepattern)  # - timedelta(hours = 1)
 
-    try: 
+    start_benchmark = pd.to_datetime(
+        benchmark_begin_loaded, unit='ms').to_pydatetime()
+    start_check = pd.to_datetime(check_begin_loaded, unit='ms').to_pydatetime()
+    # print(start_benchmark, fault_begin, fault_end)
+
+    try:
         os.mkdir("outputs/"+run+"/")
     except FileExistsError:
         print(run + " Directory exists")
     sys.stdout = open('outputs/' + run + '/latency_output.txt', 'w')
 
-    filename= path + run + "/resultsoutput.csv"
+    filename = path + run + "/resultsoutput.csv"
     producerfilename = path + run + "/produceroutput.csv"
     testdriverfilename = path + run + "/testdriverinfo.txt"
     #fig, axes = pyplot.subplots( figsize = ( 10, 5))
-    df = pd.read_csv(filename, sep=";",parse_dates=True, infer_datetime_format=True)
+    df = pd.read_csv(filename, sep=";", parse_dates=True,
+                     infer_datetime_format=True)
 
-    latency = df[[" record.timestamp", "latencylocal", " partition", "note", " window_size"]]
-    latency[' record.timestamp'] = latency[' record.timestamp'].apply(lambda ts: pd.to_datetime(ts, unit='ms').to_pydatetime())
+    latency = df[[" record.timestamp", "latencylocal",
+                  " partition", "note", " window_size"]]
+    latency[' record.timestamp'] = latency[' record.timestamp'].apply(
+        lambda ts: pd.to_datetime(ts, unit='ms').to_pydatetime())
     latency[' latency'] = latency['latencylocal']
 
     print("#Events: ", latency[" window_size"].sum())
@@ -76,8 +88,9 @@ for run in runs:
     event_count_benchmark = benchmark[" window_size"].sum()
 
     latency = check.append(benchmark)
-    
-    end_benchmark = latency[latency.note == " benchmark"][" record.timestamp"].iloc[-1]
+
+    end_benchmark = latency[latency.note ==
+                            " benchmark"][" record.timestamp"].iloc[-1]
     duration_benchmark = (end_benchmark-start_benchmark).seconds
 
     end_check = latency[latency.note == " check"][" record.timestamp"].iloc[-1]
@@ -91,84 +104,83 @@ for run in runs:
         set_axis(axis)
     pyplot.savefig('outputs/' + run + "/" + run + "_split_latency.pdf")
 
-    #average latency over partitions: 
+    # average latency over partitions:
 
-    avg_latency = latency.set_index(pd.DatetimeIndex(latency[" record.timestamp"])).sort_index()
-    avg_latency = avg_latency[[" record.timestamp", "latencylocal"]].resample("5s").mean()
+    avg_latency = latency.set_index(pd.DatetimeIndex(
+        latency[" record.timestamp"])).sort_index()
+    avg_latency = avg_latency[[" record.timestamp",
+                               "latencylocal"]].resample("5s").mean()
 
-    fig, axes = pyplot.subplots( figsize = ( 10, 5))
-    sns.lineplot(x =" record.timestamp", y="latencylocal", data=avg_latency)
-    axes.set_xticks([start_check, start_benchmark, fault_end, fault_end + timedelta(minutes=0.5)])
+    fig, axes = pyplot.subplots(figsize=(10, 5))
+    sns.lineplot(x=" record.timestamp", y="latencylocal", data=avg_latency)
+    axes.set_xticks([start_check, start_benchmark, fault_end,
+                    fault_end + timedelta(minutes=0.5)])
     axes.set_xticklabels(["t_0", "t_1   t_2", "t_3", ""])
     axes.set_ylabel("event-time latency")
     annotate()
     pyplot.savefig('outputs/' + run + "/" + run + "_latency_average.pdf")
 
-    ############ other metrics:
+    # other metrics:
 
     pattern = "%Y-%m-%d %H:%M:%S"
-    metric_path="/home/jfr/Thesis/prom-metrics/"
-    throughput_sum_file = metric_path + run + "/storm_topology_Windowbolt_Throughput_sum.csv"
+    metric_path = "/home/jfr/Thesis/prom-metrics/"
+    throughput_sum_file = metric_path + run + \
+        "/storm_topology_Windowbolt_Throughput_sum.csv"
     throughput_file = metric_path + run + "/storm_topology_Windowbolt_Throughput.csv"
     cpu_file = metric_path + run + "/node_cpu_seconds_total_1m.csv"
     #################################################################
 
     throughput_sum = pd.read_csv(throughput_sum_file, sep=",")
-    throughput_sum ["throughput"]=throughput_sum["value_1"]
-    throughput_sum['time'] = throughput_sum['time'].apply(lambda ts: datetime.strptime(ts, pattern))
+    throughput_sum["throughput"] = throughput_sum["value_1"]
+    throughput_sum['time'] = throughput_sum['time'].apply(
+        lambda ts: datetime.strptime(ts, pattern))
 
-    throughput = pd.read_csv(throughput_file, sep= ",")
-    throughput['time'] = throughput['time'].apply(lambda ts: datetime.strptime(ts, pattern))
+    throughput = pd.read_csv(throughput_file, sep=",")
+    throughput['time'] = throughput['time'].apply(
+        lambda ts: datetime.strptime(ts, pattern))
 
     cpu_util = pd.read_csv(cpu_file, sep=",")
-    cpu_util['time'] = cpu_util['time'].apply(lambda ts: datetime.strptime(ts, pattern))
+    cpu_util['time'] = cpu_util['time'].apply(
+        lambda ts: datetime.strptime(ts, pattern))
 
-    fig, axes = pyplot.subplots( figsize = (15,5))
+    fig, axes = pyplot.subplots(figsize=(15, 5))
     pyplot.subplot(121)
     annotate()
-    tp_ax = sns.lineplot(x = "time", y = "throughput", data = throughput_sum[throughput_sum.time > start_check], label = "SUM")
+    tp_ax = sns.lineplot(x="time", y="throughput",
+                         data=throughput_sum[(throughput_sum.time > start_check) & (throughput.time < (fault_end + timedelta(seconds=30)))])
     set_axis(tp_ax, "Throughput")
     #tp_ax.set_xlim(left = start_check)
-    
+
     pyplot.subplot(122)
     annotate()
-    cpu_util = cpu_util[cpu_util.time > start_check]
+    cpu_util = cpu_util[(cpu_util.time > start_check) & (cpu_util.time < (fault_end + timedelta(seconds=30)))]
     ax = sns.lineplot(x="time", y="value_1", data=cpu_util, label="Storm 1")
     set_axis(ax, "CPU Utilization")
     sns.lineplot(x="time", y="value_2", data=cpu_util, label="Storm 2")
     sns.lineplot(x="time", y="value_5", data=cpu_util, label="Storm 3")
     sns.lineplot(x="time", y="value_3", data=cpu_util, label="Kafka")
     sns.lineplot(x="time", y="value_4", data=cpu_util, label="Utilities")
-    pyplot.savefig("outputs/"+run + "/"+ run + "_metrics.pdf")
-    pyplot.show()
+    pyplot.savefig("outputs/"+run + "/" + run + "_metrics.pdf")
+    # pyplot.show()
 
     # Latency Berechnungen
-    latency_per_partition = latency.groupby(' partition')    
-    latency_partition=[latency_per_partition.get_group(x) for x in latency_per_partition.groups]    
+    latency_per_partition = latency.groupby(' partition')
+    latency_partition = [latency_per_partition.get_group(
+        x) for x in latency_per_partition.groups]
     #fig, axes = pyplot.subplots( figsize = ( 10, 5))
-    #output for latex table
+    # output for latex table
     print("Partition;AVG_control;AVG_benchmark;MAX_control;MAX_benchmark;MIN_control;MIN_benchmark;MEDIAN_control;MEDIAN_Benchmark")
     for partition in latency_partition:
         partition_num = str(partition.iloc[0][" partition"] + 1)
-        partition_check=partition[partition.note==" check"]
-        partition_benchmark=partition[partition.note==" benchmark"]
-        print(partition_num
-            , round(partition_check.mean()["latencylocal"], 2)
-            , round(partition_benchmark.mean()["latencylocal"], 2)
-            , partition_check.max()["latencylocal"]
-            , partition_benchmark.max()["latencylocal"]
-            , partition_check.min()["latencylocal"]
-            , partition_benchmark.min()["latencylocal"]
-            , partition_check.median()["latencylocal"]
-            , partition_benchmark.median()["latencylocal"]
-            , sep="&"
-            ,end=""
-            ,flush=True)
+        partition_check = partition[partition.note == " check"]
+        partition_benchmark = partition[partition.note == " benchmark"]
+        print(partition_num, round(partition_check.mean()["latencylocal"], 2), round(partition_benchmark.mean()["latencylocal"], 2), partition_check.max()["latencylocal"], partition_benchmark.max()[
+              "latencylocal"], partition_check.min()["latencylocal"], partition_benchmark.min()["latencylocal"], partition_check.median()["latencylocal"], partition_benchmark.median()["latencylocal"], sep="&", end="", flush=True)
         print("\\\\ \\hline")
         #sns.lineplot(x=" record.timestamp", y=" latency", data = partition, label = "partition " + partition_num, legend=False)
-        #marker="o"
-    
-    #setting axes ticks etc.
+        # marker="o"
+
+    # setting axes ticks etc.
     # axes.xaxis.set_major_formatter(date_fmt)
     # axes.legend(loc=5)
     # axes.set_xticks([start_check, start_benchmark, fault_end, fault_end + timedelta(minutes=0.5)])
@@ -176,57 +188,36 @@ for run in runs:
     # annotate()
     # pyplot.savefig('outputs/' + run + "/" + run + "_latency.pdf")
 
-    print("Total"
-        ,round(latency.groupby(["note"])[" latency"].mean()[" check"], 2)
-        ,round(latency.groupby(["note"])[" latency"].mean()[" benchmark"], 2)
-        ,round(latency.groupby(["note"])[" latency"].max()[" check"], 2)
-        ,round(latency.groupby(["note"])[" latency"].max()[" benchmark"], 2)
-        ,round(latency.groupby(["note"])[" latency"].min()[" check"], 2)
-        ,round(latency.groupby(["note"])[" latency"].min()[" benchmark"], 2)
-        ,round(latency.groupby(["note"])[" latency"].median()[" check"], 2)
-        ,round(latency.groupby(["note"])[" latency"].median()[" benchmark"], 2)
-        ,sep = "&"
-        ,end=""
-        ,flush=True)
+    print("Total", round(latency.groupby(["note"])[" latency"].mean()[" check"], 2), round(latency.groupby(["note"])[" latency"].mean()[" benchmark"], 2), round(latency.groupby(["note"])[" latency"].max()[" check"], 2), round(latency.groupby(["note"])[" latency"].max()[" benchmark"], 2), round(
+        latency.groupby(["note"])[" latency"].min()[" check"], 2), round(latency.groupby(["note"])[" latency"].min()[" benchmark"], 2), round(latency.groupby(["note"])[" latency"].median()[" check"], 2), round(latency.groupby(["note"])[" latency"].median()[" benchmark"], 2), sep="&", end="", flush=True)
     print("\\\\ \\hline")
 
     # TP Berechungen
     i = 0
-    throughput_check = throughput[(throughput.time > start_check) & (throughput.time < start_benchmark)]
+    throughput_check = throughput[(throughput.time > start_check) & (
+        throughput.time < start_benchmark)]
     throughput_benchmark = throughput[throughput.time >= start_benchmark]
     print("Throughput: ")
     for col in throughput.columns[2:]:
         i += 1
-        sns.lineplot(x="time", y=col, data=throughput[throughput.time > start_check], label = "Component " + str(i))
+        # sns.lineplot(
+        #     x="time", y=col, data=throughput[(throughput.time > start_check) & (throughput.time < (fault_end + timedelta(seconds=30)))], label="Component " + str(i))
         partition_check = throughput_check[col][throughput_check[col] > 0]
         partition_benchmark = throughput_benchmark[col][throughput_benchmark[col] > 0]
-        print(col
-            ,partition_check.mean()
-            ,partition_benchmark.mean()
-            ,partition_check.max()
-            ,partition_benchmark.max()
-            ,partition_check.min()
-            ,partition_benchmark.min()
-            ,partition_check.median()
-            ,partition_benchmark.median()
-            ,sep = " & ")
+        print(col, partition_check.mean(), partition_benchmark.mean(), partition_check.max(), partition_benchmark.max(
+        ), partition_check.min(), partition_benchmark.min(), partition_check.median(), partition_benchmark.median(), sep=" & ")
 
-    throughput_melted = throughput.melt( id_vars = ["time", "id"] , var_name = "Component" , value_name = "Throughput")
-    throughput_check =  throughput_melted[(throughput_melted.time > start_check) & (throughput_melted.time < start_benchmark) & (throughput_melted.Throughput > 0)]
-    throughput_benchmark = throughput_melted[(throughput_melted.time >= start_benchmark) & (throughput_melted.Throughput > 0)]
+    throughput_melted = throughput.melt(
+        id_vars=["time", "id"], var_name="Component", value_name="Throughput")
+    throughput_check = throughput_melted[(throughput_melted.time > start_check) & (
+        throughput_melted.time < start_benchmark) & (throughput_melted.Throughput > 0)]
+    throughput_benchmark = throughput_melted[(
+        throughput_melted.time >= start_benchmark) & (throughput_melted.Throughput > 0)]
 
-    print("Total"
-        ,throughput_check.mean()["Throughput"]
-        ,throughput_benchmark.mean()["Throughput"]
-        ,throughput_check.max()["Throughput"]
-        ,throughput_benchmark.max()["Throughput"]
-        ,throughput_check.min()["Throughput"]
-        ,throughput_benchmark.min()["Throughput"]
-        ,throughput_check.median()["Throughput"]
-        ,throughput_benchmark.median()["Throughput"]
-        ,sep = " & "
-        )
+    print("Total", throughput_check.mean()["Throughput"], throughput_benchmark.mean()["Throughput"], throughput_check.max()["Throughput"], throughput_benchmark.max()["Throughput"], throughput_check.min()["Throughput"], throughput_benchmark.min()["Throughput"], throughput_check.median()["Throughput"], throughput_benchmark.median()["Throughput"], sep=" & "
+          )
 
     calc_throughput_check = event_count_check / duration_check
     calc_throughput_benchmark = event_count_benchmark / duration_benchmark
-    print("Calculted total throughputs: \ncheck: ", calc_throughput_check,"benchmark: ", calc_throughput_benchmark, "Diff: ", round(((calc_throughput_benchmark - calc_throughput_check) / calc_throughput_check) * 100, 2), "%")
+    print("Calculted total throughputs: \ncheck: ", calc_throughput_check, "benchmark: ", calc_throughput_benchmark,
+          "Diff: ", round(((calc_throughput_benchmark - calc_throughput_check) / calc_throughput_check) * 100, 2), "%")
